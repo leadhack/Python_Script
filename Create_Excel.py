3aee8cfad02fda3c0101a782ab313462996c4533
LeadHack <103206072+LeadHack@users.noreply.github.com>
2023-12-08 15:55:20 +0100

    Update Create_Excel.py

--- Create_Excel.py
+++ Create_Excel.py
@@ -3,19 +3,15 @@ import openpyxl
 def creer_fichier_excel(nom_fichier):
     # Créer un nouveau classeur Excel
     classeur = openpyxl.Workbook()
-
     # Sélectionner la feuille active
     feuille = classeur.active
-
     # Ajouter des données de démonstration (vous pouvez personnaliser cela selon vos besoins)
     feuille['A1'] = 'Id Employe'
     feuille['B1'] = 'Prenom'
     feuille['C1'] = 'Nom'
     feuille['D1'] = 'Poste'
-
     # Sauvegarder le fichier Excel avec le nom spécifié
     classeur.save(nom_fichier)
-
     print(f'Le fichier Excel "{nom_fichier}" a été créé avec succès.')
 
 # Exemple d'utilisation de la fonction
