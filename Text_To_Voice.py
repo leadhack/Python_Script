c6065182daa114d2c7dbabc55ecebee3d6929f6a
LeadHack <103206072+LeadHack@users.noreply.github.com>
2023-12-09 23:39:39 +0100

    Update Text_To_Voice.py

--- Text_To_Voice.py
+++ Text_To_Voice.py
@@ -1,1 +1,16 @@
+#pip install gTTs
+from gtts import gTTS
+import os
+
+# Votre texte à transformer en voix
+texte = "Découvrez la puissance de la technologie vocale avec gTTS et Python!"
+
+# Créez un objet gTTS
+tts = gTTS(text=texte, lang='fr', slow=False)
+
+# Sauvegardez le fichier audio
+tts.save("description_audio.mp3")
+
+# Lancez le lecteur audio pour écouter la description
+os.system("start description_audio.mp3")
 
